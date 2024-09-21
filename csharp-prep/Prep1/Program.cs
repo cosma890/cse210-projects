97b40747297391645b5b40e3c12760275fa14771
cosma890 <hernancosma@gmail.com>
2024-09-21 16:08:36 +0200

    Promp and display

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your lastname? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {name}, {lastname}.");
+        
     }
 }

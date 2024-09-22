48aeb4b65efcf26f3394b286942485a5a7e2598a
cosma890 <hernancosma@gmail.com>
2024-09-22 18:48:23 +0200

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,35 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {  // First part
+      //Console.Write("What is your magic number? ");
+      //int response = int.Parse(Console.ReadLine());
+
+        // Second part
+        Random randomGenerator = new Random();
+        int theNumber = randomGenerator.Next(1, 10);
+    
+
+        int guess = -1;
+
+        while (guess != theNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (theNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (theNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }

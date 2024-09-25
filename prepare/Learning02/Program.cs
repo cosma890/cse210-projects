da7d1b9f5ea47452ef2464b8bcf281877b8d3f9a
cosma890 <hernancosma@gmail.com>
2024-09-25 14:48:41 +0200

    Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Programmer";
+        job1._company = "Google";
+        job1._startYear = 2018;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Junior Developer";
+        job2._company = "IBM";
+        job2._startYear = 2021;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Ashley Parson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
+
